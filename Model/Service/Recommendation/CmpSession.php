bb59f77c78d815b436fe1ab2099c2f4339e5e1be
Hannu Pölönen <hannu.polonen@nosto.com>
2020-10-23 16:27:10 +0300

    Remove unusued constant & update SDK version constraint

--- Model/Service/Recommendation/CmpSession.php
+++ Model/Service/Recommendation/CmpSession.php
@@ -41,8 +41,6 @@ use Magento\Framework\Session\SessionManagerInterface;
 
 class CmpSession
 {
-    const SESSION = 'nosto_cmp_session';
-
     /** @var SessionManagerInterface */
     private $session;
 
