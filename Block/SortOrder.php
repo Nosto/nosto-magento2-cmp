e84eb6dfe84e2dcd0731e37ecf82b81774a56311
Dair Baidauletov <dair.baidauletov@gmail.com>
2021-12-23 16:05:03 +0200

    Remove unneeded no-inspect suspension

--- Block/SortOrder.php
+++ Block/SortOrder.php
@@ -38,7 +38,7 @@ namespace Nosto\Cmp\Block;
 
 use Magento\Catalog\Model\Category;
 use Magento\Framework\App\Request\Http;
-use /** @noinspection PhpDeprecationInspection */Magento\Framework\Registry;
+use /** @noinspection PhpDeprecationInspection */ Magento\Framework\Registry;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Nosto\Cmp\Helper\CategorySorting;
@@ -60,7 +60,6 @@ class SortOrder extends Template
 
     /** @var Registry */
     private $registry;
-    /** @noinspection PhpDeprecationInspection */
 
     /**
      * SortOrder constructor.
