a143f71a4cddf17e39f90c3e6446e403818834bc
Dair Baidauletov <dair.baidauletov@gmail.com>
2022-04-04 13:59:02 +0300

    Add exception if Magento is set to use MySQL as a search engine with CMP version 4

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Copyright (c) 2020, Nosto Solutions Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Nosto Solutions Ltd <contact@nosto.com>
+ * @copyright 2020 Nosto Solutions Ltd
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ *
+ */
+
+namespace Nosto\Cmp\Setup;
+
+use Exception;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Nosto\Cmp\Helper\SearchEngine;
+use Nosto\Cmp\Exception\SearchEngineException;
+use Nosto\Tagging\Logger\Logger;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /** @var SearchEngine */
+    private $searchEngine;
+
+    /** @var Logger */
+    private $logger;
+
+    /**
+     * UpgradeData constructor.
+     * @param SearchEngine $searchEngine
+     * @param Logger $logger
+     */
+    public function __construct(
+        SearchEngine $searchEngine,
+        Logger $logger
+    ) {
+        $this->searchEngine = $searchEngine;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @throws SearchEngineException
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context) // @codingStandardsIgnoreLine
+    {
+        $fromVersion = $context->getVersion();
+        if (version_compare($fromVersion, '4.0.0', '<=')) {
+            $this->checkSearchEngine();
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function checkSearchEngine()
+    {
+        if ($this->searchEngine->isMysql()) {
+            throw new Exception(
+                'MySQL search engine is not supported starting from version 4.0.0. ' .
+                'Downgrade module version or switch to Elasticsearch engine.');
+        }
+    }
+}
